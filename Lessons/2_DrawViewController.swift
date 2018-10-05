9e00e95ca87889c46d334b748a79efdbb7a118d8
Lammax <lammax@mail.ru>
2018-10-05 18:11:53 +0300

    Lesson 2 Added camera's position & orientation

--- Lessons/2_DrawViewController.swift
+++ Lessons/2_DrawViewController.swift
@@ -37,7 +37,16 @@ class DrawViewController: UIViewController {
 extension DrawViewController: ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
-        print("rendering at time \(time)")
+        guard let pointOfView = self.sceneView.pointOfView else { return }
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31, -transform.m32, transform.m33)
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let frontOfCamera = orientation + location
+        print( orientation.x, orientation.y, orientation.z )
     }
     
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
 }
