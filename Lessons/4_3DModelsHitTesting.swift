83770abf0e2eb7f0803c349619ea9523c32b25e1
Lammax <lammax@mail.ru>
2018-10-24 19:44:01 +0300

    Add some images for planets Add 4th lesson for hit testing Added tapGestureRecognizer

--- Lessons/4_3DModelsHitTesting.swift
+++ Lessons/4_3DModelsHitTesting.swift
@@ -23,11 +23,41 @@ class ModelsHitTestingViewController: UIViewController {
         self.sceneView.session.run(configuration)
         self.sceneView.autoenablesDefaultLighting = true
         self.sceneView.delegate = self
+        
+        self.addTapGesture(view: self.sceneView)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         self.sceneView.session.pause()
     }
+    
+    @IBAction func clickPlayButton(_ sender: UIButton) {
+        self.addNode()
+    }
+    
+    @IBAction func clickResetButton(_ sender: UIButton) {
+    }
+    
+    func addTapGesture(view: ARSCNView) {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func handleTap() {
+        print("Scene view tapped")
+    }
+    
+    func addNode() {
+        
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        
+        node.position = SCNVector3(0, 0, -1)
+        
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+    }
+    
+    
 }
 
 extension ModelsHitTestingViewController: ARSCNViewDelegate {
